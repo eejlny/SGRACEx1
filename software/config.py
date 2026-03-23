a208196c0c358ef06e5740f16d9e7b691edb5ce0
eejlny <34302344+eejlny@users.noreply.github.com>
2026-03-23 12:45:45 +0100

    Update config.py

--- software/config.py
+++ software/config.py
@@ -7,9 +7,6 @@ write_file = 0 #write file for debugging
 instant_layer_count = 1 #how many layers to process in each SGRACE (core) hardware call.
 total_layer_count = 3 #how many total SGRACE layers.
 
-#instant_layer_count = 3 #how many layers to process in each SGRACE (core) hardware call.
-#total_layer_count = 3 #how many total SGRACE layers.
-
 core_count = 1 #how mny cores in dataflow mode avaialble in the hardware configuration
 load_weights = 1 #load weights into hardware (needed for training, inference can just load once and then reuse)
 
@@ -33,27 +30,10 @@ P_w =  hidden_channels #hid number hidden chnnels
 NNZ_adj = 1000000 # max number of non-zero values of adjacency
 NNZ_fea = 4000000 # max number of non-zero values of feature
 w_qbits = 8#1
-#w_qbitsl = 8#4
 w_qbitsl = 8#flaot
 
-#global hard_type
-#hard_type = np.int8
-#global out_type
-#out_type = np.int32
-#global float_type
+
 float_type = np.float32
 model_type = np.uint8 
 
-#global layern
-
-#global rowPtr_adj_buffer
-#rowPtr_adj_buffer = []
-#global values_adj_buffer
-#values_adj_buffer = []
-#global rowPtr_fea_buffer
-#rowPtr_fea_buffer = []
-#global columnIndex_fea_buffer
-#columnIndex_fea_buffer = []
-#global values_fea_buffer
-#values_fea_buffer = []
 
